1cf9defdb40d87feca8c3f84dcf89a266eb4b87e
James Kim <jamesmankim@Gmail.com>
2016-02-05 08:27:27 -0500

    Finished CM SM rev 1

--- CB_SM.ino
+++ CB_SM.ino
@@ -1,61 +1,138 @@
-#define REL1 7
-#define REL2 6
-#define REL3 5
-#define REL4 4
-#define BTN	 3
-#define LEDST 2
+// Import NeoPixel library to initialize pixel 
+#define VALV_PMP 7
+#define VALV_CBR 6
+#define VAVL_ATM 5
+#define PUMP	4
+#define BTN 3
+#define PRS_SEN 8
 
-enum states {idle, vacu, dwll, drin}
+// Different states of the machine: Idle, Vacuum, Dwell, and Drain
+enum states {IDLE, VACU, DWLL, DRIN}
+
+unsigned long startTime;
+
+// Machine of various 'states' and updating method
 class Machine
 {
     // Class Member Variables
-    // These are initialized at startup
-	int state;	//	Default state is idle
-    long vacDwlTime;    // milliseconds of off-time
+	volatile int state;						// Current state of the machine
+	long vacDwlTime;    					// Time limit for Vacuum and Dwell state combined
+	int updateInterval; 					// Update interval for checking for changed button state
  
-  // Constructor - creates a Flasher 
-  // and initializes the member variables and state
-  public:
-  Machine(int startState, long setTime)
-  {
-    ledPin = pin;
-    pinMode(ledPin, OUTPUT);     
-      
-    OnTime = on;
-    OffTime = off;
-    
-    ledState = LOW; 
-    previousMillis = 0;
-  }
- 
-  void Update(unsigned long currentMillis)
-  {
-    if((ledState == HIGH) && (currentMillis - previousMillis >= OnTime))
-    {
-        ledState = LOW;  // Turn it off
-      previousMillis = currentMillis;  // Remember the time
-      digitalWrite(ledPin, ledState);  // Update the actual LED
-    }
-    else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime))
-    {
-      ledState = HIGH;  // turn it on
-      previousMillis = currentMillis;   // Remember the time
-      digitalWrite(ledPin, ledState);     // Update the actual LED
-    }
-  }
+ 	// Constructor for Machine class
+  	public:
+  	Machine(int startState, long setTime, int interval = 20)
+  	{
+		state = startState;
+  	  	vacDwlTime = setTime;
+		updateInterval = interval;
+  	}
 };
+
+Machine coffee(IDLE, 300000); 
+volatile int* volatile ptr = coffee.state;
 
 void setup() {
 	Serial.begin(9600);
 
-	pinMode(REL1, OUTPUT);
-	pinMode(REL2, OUTPUT);
-	pinMode(REL3, OUTPUT);
-	pinMode(REL4, OUTPUT);
+	pinMode(VALV_PMP, OUTPUT);
+	pinMode(VALV_CBR, OUTPUT);
+	pinMode(VALV_ATM, OUTPUT);
+	pinMode(PUMP, OUTPUT);
+
+	pinMode(PRS_SEN, INPUT);
+	
+	pinMode(BTN, INPUT_PULLUP);
+	attachINterrupt(BTN, changeState, FALLING);
 }
 
-void loop() {
+void changeState() {
+	switch (coffee.state)
+	{
+		case IDLE:
+			coffee.state = VACU;
+			//	Idle -> Vacuum
+			break;
+		case VACU:
+			coffee.state = DRIN;
+			//	Vacuum -> Drain
+			break;
+		case DWLL:
+			coffee.state = DRIN;
+			//	Dwell -> Drain
+			break;
+		case DRIN:
+			coffee.state = IDLE
+			//	Drain -> Idle
+			break;
+		default:
+			break;
+	}
+	ptr = &coffee.state;
+}
 
+
+void loop() {
+	// Execute methods due to volatile state changes
+	
+
+	switch (*ptr)
+	{
+		case IDLE:
+			idle();
+			ptr = NULL;
+			break;
+
+		case VACU:
+			vacuum();
+			ptr = NULL;
+			break;
+
+		case DWLL:
+			dwell();
+			ptr = NULL;
+			break;
+
+		case DRIN:
+			drain();
+			ptr = NULL;
+			break;
+	}
+
+	if ((*ptr == VACU || *ptr == DWLL) && (millis() - startTime) > coffee.vacDwlTime) {
+		coffee.state = DRIN;
+	} else if ((millis() - startTime) <= coffee.vacDwlTime && *ptr == VACU && digitalRead(PRS_SEN) == HIGH) {
+		coffee.state = DWLL;
+	} else if ((millis() - startTime) <= coffee.vacDwlTime && *ptr == DWLL && digitalRead(PRS_SEN) == LOW) {
+		coffee.state = VACU;
+	}
+}
+
+void idle() {
+	led.clear();
+}
+
+void vacuum() {
+	startTime = millis();
+	digitalWrite(VALV_PUMP, HIGH);
+	led.strip(0, led.strip.Color(125, 125, 0))
+	digitalWrite(PUMP, HIGH);
+}
+
+void dwell() {
+	digitalWrite(VALV_PUMP, LOW);
+	digitalWrite(PUMP, LOW);
+	digitalWrite(VALV_CBR, HIGH);
+
+	led.strip(0, led.Color(255, 0, 0));
+}
+
+void drain() {
+	digitalWrite(VALV_PUMP, LOW);
+	digitalWrite(VALV_CBR, LOW);
+	digitalWrite(VALV_ATM, HIGH);
+
+	led.strip(0, led.Color(0, 255, 0));
 }
 
 
